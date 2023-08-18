421a4ee9b4feb5533ba67adfa7e327a599b6830a
Fibi Youssef <fibi057057@gmail.com>
2023-08-17 20:58:02 -0500

    adding API

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const initialFacts = [
     category: "technology",
     votesInteresting: 24,
     votesMindblowing: 9,
-    votesFalse: 2,
+    votesFalse: 4,
     createdIn: 2021,
   },
   {
     id: 2,
-    text: "Millenial dads spend 3 times as much time with their kids than their fathers spent with them. IN 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
     source:
-      "https://www.mother.ly/parenting/millenial-dads-spend-more-time-with-their-kids",
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
     category: "society",
     votesInteresting: 11,
     votesMindblowing: 2,
@@ -32,22 +32,70 @@ const initialFacts = [
   },
 ];
 
-// selecting DOM elements
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+// Selecting DOM elements
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
-const factsList = document.querySelector("facts-list");
+const factsList = document.querySelector(".facts-list");
 
-// create DOM elements: render facts in list
+// Create DOM elements: Render facts in list
 factsList.innerHTML = "";
 
-const htmlArr = initialFacts.map(
-  (fact) => `<li class="fact">${fact.text}</li>`
-);
-console.log(htmlArr);
-const html = htmlArr.join("");
-factsList.insertAdjacentHTML("afterbegin", html);
+// Load data from Supabase
+loadFacts();
 
-// toggle form visibility
+async function loadFacts() {
+  const res = await fetch(
+    "https://hwtkikrqvnsrrpkjrytv.supabase.co/rest/v1/facts",
+    {
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3dGtpa3Jxdm5zcnJwa2pyeXR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg3NTk3ODUsImV4cCI6MTk4NDMzNTc4NX0.l_PGJRds8Lmg17Pn7eqNwFqYWH4rST2gVlTeGCfKTjk",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3dGtpa3Jxdm5zcnJwa2pyeXR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg3NTk3ODUsImV4cCI6MTk4NDMzNTc4NX0.l_PGJRds8Lmg17Pn7eqNwFqYWH4rST2gVlTeGCfKTjk",
+      },
+    }
+  );
+  const data = await res.json();
+  // console.log(data);
+  // const filteredData = data.filter((fact) => fact.category === "technology");
+
+  createFactsList(data);
+}
+
+function createFactsList(dataArray) {
+  // factsList.insertAdjacentHTML("afterbegin", "<li>Jonas</li>");
+
+  const htmlArr = dataArray.map(
+    (fact) => `<li class="fact">
+    <p>
+    ${fact.text}
+      <a
+        class="source"
+        href="${fact.source}"
+        target="_blank"
+      >(Source)</a>
+    </p>
+    <span class="tag" style="background-color: ${
+      CATEGORIES.find((cat) => cat.name === fact.category).color
+    }">${fact.category}</span>
+  </li>`
+  );
+  const html = htmlArr.join("");
+  factsList.insertAdjacentHTML("afterbegin", html);
+}
+
+// Toggle form visibility
 btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
@@ -58,25 +106,5 @@ btn.addEventListener("click", function () {
   }
 });
 
-/*
-function calcFactAge(year) {
-  const currentYear = new Date().getFullYear();
-  const age = currentYear - year;
-
-  if (age >= 0) return age;
-  else return `Impossible year. Year needs to be less or equal ${currentYear}`;
-}
-
-const age1 = calcFactAge(2015);
-console.log(age1);
-console.log(calcFactAge(2020));
-console.log(calcFactAge(2037));
-
-const calcFactAge2 = (year) =>
-  year <= new Date().getFullYear()
-    ? new Date().getFullYear() - year
-    : `Impossible year. Year needs to be less or equal ${new Date().getFullYear()}`;
-
-console.log(calcFactAge2(2015));
-console.log(calcFactAge2(2037));
-*/
+console.log([7, 64, 6, -23, 11].filter((el) => el > 10));
+console.log([7, 64, 6, -23, 11].find((el) => el > 10));
